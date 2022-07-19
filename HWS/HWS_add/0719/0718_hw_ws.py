9fd136de96bd41cda45c74947d23c8a596c2a139
Lee-hanbin <hanbin0501@gmail.com>
2022-07-19 17:28:12 +0900

    complete hw_0719

---
+++ HWS/HWS_add/0719/0718_hw_ws.py
@@ -0,0 +1,19 @@
+# #1
+
+# print('It\'s SSAFY 8')
+
+# #2 
+
+# print(458345 + 623576)
+
+# #3
+
+# greeting = 'Hello'
+# month = 'July'
+
+# print(greeting + ' ' + month)
+
+# #4 
+
+# hello = input()
+# print(hello)
---
+++ HWS/HWS_add/0719/0719_hw_ws.py
@@ -0,0 +1,17 @@
+# #1
+
+# num_1 = int(input('첫 번째 입력값 : '))
+# num_2 = int(input('두 번째 입력값 : '))
+
+# print(num_1 + num_2)
+
+# #2
+
+# dict_a = {'초밥':20000,'파스타':15000}
+
+# sum = 0
+
+# for i in dict_a.values():
+#     sum += i
+
+# print(sum/len(dict_a.keys()))
