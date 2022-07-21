a792b7dd214916b0336fdd6f18cd1274a0f8d2f1
Lee-hanbin <hanbin0501@gmail.com>
2022-07-21 18:00:23 +0900

    update 0721

---
+++ python/HWS_lab/0721/0721_hw_lab.py
@@ -0,0 +1,11 @@
+# #실습 4_3
+# lst = [1, 1, 3, 3, 0, 1, 1]
+# lst_new = []
+# for i in range(0,len(lst)):
+#     if i == 0:
+#         lst_new.append(lst[i])
+#     elif lst[i] == lst[i-1]:
+#         Continue
+#     else:
+#         lst_new.append(lst[i])
+# print(lst_new)
