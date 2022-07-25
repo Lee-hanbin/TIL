143be6d1d7f037625807be25240fed0f4cec0eb6
Lee-hanbin <hanbin0501@gmail.com>
2022-07-25 18:30:11 +0900

    update homework_0725

---
+++ python/HWS_bonus/0725/0725_hw.py
@@ -0,0 +1,28 @@
+# #1 
+
+# def conut_vowels(string):
+#     sum = string.count('a') + string.count('e') + string.count('i') + string.count('o') + string.count('u')
+#     return sum
+
+# print(conut_vowels('apple'))
+# print(conut_vowels('banana'))
+
+# #2
+
+# #  (1)  맞음
+# #  (2)  맞음
+# #  (3)  맞음
+# #  (4)  특정 문자를 지정하지 않으면 오류 발생 x , 공백을 기준으로 나눠서 반환
+
+
+# #3
+
+# def only_square_area(lst_1, lst_2):
+#     lst_sol = []
+#     for i in lst_1:
+#         for j in lst_2:
+#             if i == j:
+#                 lst_sol.append(i**2)
+#     return lst_sol            
+
+# print(only_square_area([32, 55, 63], [13, 32, 40, 55]))
---
+++ python/HWS_lab/0725/0725_hw_lab.py
@@ -0,0 +1,11 @@
+# #과제 5_2
+
+# def leap_year(year):
+#     if year % 4 == 0 and year % 100 != 0:
+#         print(f'{year}년은 윤년입니다.')
+#     elif year % 400 == 0:   
+#         print(f'{year}년은 윤년입니다.')
+#     else:
+#         print(f'{year}년은 윤년이 아닙니다.')
+            
+        
