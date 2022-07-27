9bf7b5ce481c45f85ce8d5ddf19ff4880ea54d75
Lee-hanbin <hanbin0501@gmail.com>
2022-07-28 02:25:54 +0900

    update 0727_lab

--- python/HWS_lab/0727/0727_lab.py
+++ python/HWS_lab/0727/0727_lab.py
@@ -8,10 +8,26 @@ class Doggy():
         self.name = name
         self.breed = breed
         Doggy.num_of_dogs += 1
-        Doggy.burth_of_dogs += 1
+        Doggy.birth_of_dogs += 1
     
-    def bark():
+    def die(self):
+        Doggy.num_of_dogs -= 1
+
+    def bark(self):
         print('월월')
     
     @classmethod
-    def get_status()
+    def get_status(cls):
+        print(f'태어난 개의 수 : {cls.birth_of_dogs}, 현재 있는 개의 수 : {cls.num_of_dogs}')
+
+
+Doggy1 = Doggy('멍멍','말티즈')
+Doggy1.bark()
+Doggy2 = Doggy('멍멍멍','푸들')
+Doggy2.bark()
+Doggy3 = Doggy('멍', '치와와')
+Doggy3.bark()
+Doggy.get_status()
+Doggy3.bark()
+Doggy3.die()
+Doggy.get_status()
