baba257c1611439f8ad7149d62b72f4d3ac30e0f
Lee-hanbin <hanbin0501@gmail.com>
2022-07-30 13:20:12 +0900

    update lotto_0728

---
+++ python/practice/0728/lotto.py
@@ -0,0 +1,43 @@
+# 여기에 필요한 모듈을 추가합니다.
+
+
+class Lotto:
+    # 2-2. 생성자 작성
+    def __init__(self):
+        pass
+
+    # 2-3. n 줄의 로또 번호를 생성하는 인스턴스 메서드
+    def generate_lines(self, n):
+        pass
+
+    # 3-2. 회차, 추첨일, 로또 번호 정보를 출력하는 인스턴스 메서드
+    def print_number_lines(self, draw_number):
+        pass
+
+    # 4-2. 해당 회차의 당첨 번호와 당첨 결과를 출력하는 인스턴스 메서드
+    def print_result(self, draw_number):
+        pass
+
+    # 3-3. 해당 회차 추첨일의 년, 월, 일 정보를 튜플로 반환하는 스태틱 메서드
+    @staticmethod
+    def get_draw_date(draw_number):
+        pass
+        # return year, month, day
+
+    # 4-3. 해당 회차 당첨 번호의 메인 번호와 보너스 번호가 담긴 튜플을 반환하는 스태틱 메서드
+    @staticmethod
+    def get_lotto_numbers(draw_number):
+        pass
+        # return main_numbers, bonus_number
+
+    # 4-4. 한 줄의 로또 번호와 메인 번호가 일치하는 개수와 보너스 번호 일치 여부가 담긴 튜플을 반환하는 스태틱 메서드
+    @staticmethod
+    def get_same_info(main_numbers, bonus_number, line):
+        pass
+        # return same_main_counts, is_bonus
+
+    # 4-5. 당첨 결과를 정수로 반환하는 스태틱 메서드
+    @staticmethod
+    def get_ranking(same_main_counts, is_bonus):
+        pass
+        # return ranking
