16b97c8849c5d28aa0e32a50a7fe7f8e572e436f
Lee-hanbin <hanbin0501@gmail.com>
2022-10-01 23:57:02 +0900

    algo

--- algorithm/SWEA/0929/SWEA_14282_d3-최소비용/SWEA_14282_d3-최소비용.py
+++ algorithm/SWEA/0929/SWEA_14282_d3-최소비용/SWEA_14282_d3-최소비용.py
@@ -4,6 +4,9 @@ from collections import deque
 import sys
 sys.stdin = open('sample_input(3).txt')
 
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+
 def bfs():
     queue = deque()
     queue.append((0,0,0))
@@ -11,27 +14,21 @@ def bfs():
     while queue:
         w, r, c = queue.popleft()
         h = lst[r][c]                       # 현재 높이
-        dr = [-1, 0, 1, 0]
-        dc = [ 0, 1, 0,-1]
         for i in range(4):
             tmp = w
             nr = r + dr[i]
             nc = c + dc[i]
             if 0<=nr<=n-1 and 0<=nc<=n-1:
+                tmp += 1
                 if h < lst[nr][nc]:
-                    tmp += 1 + (lst[nr][nc] - h)
-                else:
-                    tmp += 1
-            else:
-                continue
-            if visited[nr][nc] > tmp:
-                visited[nr][nc] = tmp
-                queue.append((tmp, nr, nc))
+                    tmp += lst[nr][nc] - h
+                if visited[nr][nc] > tmp:
+                    visited[nr][nc] = tmp
+                    queue.append((tmp, nr, nc))
 
 for t in range(int(input())):
     n = int(input())
     lst = [list(map(int, input().split())) for i in range(n)]
-
     visited = [[float('inf')] * n for _ in range(n)]
     bfs()
     print(f'#{t+1} {visited[n-1][n-1]}')
