3d8e3174b26fa4949e0c8c68702a1f03b4109956
Lee-hanbin <hanbin0501@gmail.com>
2022-11-03 18:01:53 +0900

    up

---
+++ vue/vue-cli/src/components/MyComponentItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>나는 MyComponent의 하위 컴포넌트!</h3>
+    <p>{{ staticProps }}</p>
+    <p>{{ dynamicProps }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponentItem',
+  // 2. 선언
+  props: {
+    staticProps: String,
+    dynamicProps: String,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vue문법/1101_실습/01.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- htML -->
+  <div>
+    <p>Hello :( </p>
+  </div>
+</template>
+
+<script>
+  // JS
+</script>
+
+<style >
+  /* CSS */
+  p {
+    color: red;
+  }
+</style>
