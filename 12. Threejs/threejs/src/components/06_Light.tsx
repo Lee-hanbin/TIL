65538271c70dd01e86d8f353006d758af12ff6e8
Lee-hanbin <hanbin0501@gmail.com>
2023-04-21 18:00:55 +0900

    :memo: Light 작업중

--- 12. Threejs/threejs/src/components/06_Light.tsx
+++ 12. Threejs/threejs/src/components/06_Light.tsx
@@ -7,24 +7,83 @@ const Light = () => {
   const renderer = useRef<THREE.WebGLRenderer | null>(null);
   const scene = useRef<THREE.Scene | null>(null);
   const camera = useRef<THREE.PerspectiveCamera | null>(null);
-  const cube = useRef<THREE.Mesh | null>(null);
+  const lightRef = useRef<THREE.AmbientLight | null >(null)
 
   /** 카메라 커스텀 함수 */
   const SetupCamera = () => {
     const width = divContainer.current?.clientWidth || 0;
     const height = divContainer.current?.clientHeight || 0;
     const cam = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-    cam.position.z = 2;
+    
+    // cam.position.z = 2;
+    // 카메라 위치 조정
     camera.current = cam;
   };
 
   /** 조명 커스텀 함수 */
   const SetupLight = () => {
+    const light = new THREE.AmbientLight(0xffffff, 5)
 
+    scene.current?.add(light);
+    lightRef.current = light
   };
 
   /** 모델 커스텀 함수 */
   const SetupModel = () => {
+    // ground
+    const groundGeometry = new THREE.PlaneGeometry(10, 10);
+    const groundMaterial = new THREE.MeshStandardMaterial({
+      color: "#2c3e50",
+      roughness: 0.5,
+      metalness: 0.5,
+      side: THREE.DoubleSide
+    });
+
+    const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    ground.rotation.x = THREE.MathUtils.degToRad(-90);
+    scene.current?.add(ground);
+
+    // BigSphereGeometry
+    const bigSphereGeometry = new THREE.SphereGeometry(1.5, 64, 64, 0, Math.PI);
+    const bigSphereMaterial = new THREE.MeshStandardMaterial({
+      color: "#ffffff",
+      roughness: 0.1,
+      metalness: 0.2,
+    });
+    const bigSphere = new THREE.Mesh(bigSphereGeometry, bigSphereMaterial);
+    bigSphere.rotation.x = THREE.MathUtils.degToRad(-90);
+    scene.current?.add(bigSphere);
+
+    //Torus
+    const torusGeometry = new THREE.TorusGeometry(0.4, 0.1, 32, 32);
+    const torusMaterial = new THREE.MeshStandardMaterial({
+      color: "#9b59b6",
+      roughness: 0.5,
+      metalness: 0.9,
+    });
+
+    for (let i=0; i<8; i++) {
+      const torusPivot = new THREE.Object3D();
+      const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+      torusPivot.rotation.y = THREE.MathUtils.degToRad(45 * i);
+      torus.position.set(3, 0.5, 0);
+      torusPivot.add(torus);
+      scene.current?.add(torusPivot);
+    }
+
+    //SmallSphere
+    const smallShphereGeometry = new THREE.SphereGeometry(0.3, 32, 32);
+    const smallShphereMaterial = new THREE.MeshStandardMaterial({
+      color: "#e74c3c",
+      roughness: 0.2,
+      metalness: 0.5
+    });
+    const smallSpherePivot = new THREE.Object3D();
+    const smallSphere = new THREE.Mesh(smallShphereGeometry, smallShphereMaterial);
+    smallSpherePivot.add(smallSphere);
+    smallSpherePivot.name = "smallSpherePivot";
+    smallSphere.position.set(3, 0.5, 0);
+    scene.current?.add(smallSpherePivot);
 
   };
 
