cd27893fc19822cd1afd0563f828f04cfd7617ad
Lee-hanbin <hanbin0501@gmail.com>
2023-04-23 23:58:45 +0900

    :memo: Camera 구현
    - PerspectiveCamera 와 OrthographicCamera
    
    - PerspectiveCamera 객체 추적 기능 구현

--- 12. Threejs/threejs/src/components/07_Camera.tsx
+++ 12. Threejs/threejs/src/components/07_Camera.tsx
@@ -10,7 +10,7 @@ const Camera = () => {
   const divContainer = useRef<HTMLDivElement>(null);
   const renderer = useRef<THREE.WebGLRenderer | null>(null);
   const scene = useRef<THREE.Scene | null>(null);
-  const camera = useRef<THREE.PerspectiveCamera | null>(null);
+  const camera = useRef<THREE.OrthographicCamera | THREE.PerspectiveCamera | null>(null);
 
   const lightRef = useRef<THREE.RectAreaLight | null >(null)
   const lighthelperRef = useRef<RectAreaLightHelper | null>(null)
@@ -19,10 +19,25 @@ const Camera = () => {
 
   /** 카메라 커스텀 함수 */
   const SetupCamera = () => {
-    const width = divContainer.current?.clientWidth || 0;
-    const height = divContainer.current?.clientHeight || 0;
-    const cam = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+
+    //1. PerspectiveCamera
+    const cam = new THREE.PerspectiveCamera(
+      75, //fovy ( 단위는 radian 아니고 degree )
+      window.innerWidth / window.innerHeight, //aspect 
+      0.1, //zNear
+      100  //zFar
+    );
     
+    // //2. OrthographicCamera
+    // const aspect = window.innerWidth / window.innerHeight;
+    // const cam = new THREE.OrthographicCamera(
+    //   -1*aspect, 1*aspect,  //xLeft, xRight
+    //   1, -1,    //yTop, yBottom
+    //   0.1, 100  //zNear, zFar 
+    // )
+    
+    // cam.zoom = 0.15;
+
     // cam.position.z = 2;
     // 카메라 위치 조정
     cam.position.set(7,7,0);      // 카메라의 위치는 7, 7, 0
@@ -105,18 +120,40 @@ const Camera = () => {
     smallSphere.position.set(3, 0.5, 0);
     scene.current?.add(smallSpherePivot);
 
+
+    // 작은 공을 추적하기 위해 객체를 만듦 ( 작은 구가 바라보는 화면을 카메라로 전환하기 위해 )
+    const targetPivot = new THREE.Object3D();
+    const target = new THREE.Object3D();  // Mesh 대신에 Object3D이므로 화면상에 보이지는 않음
+    targetPivot.add(target);
+    targetPivot.name = "targetPivot";
+    target.position.set(3, 0.5, 0);
+    scene.current?.add(targetPivot);
+
   };
 
   /** 렌더링 될 때마다 사이즈 초기화 */
   const resize = () => {
     const width = divContainer.current?.clientWidth || 0;
     const height = divContainer.current?.clientHeight || 0;
+    const aspect = width / height;
 
-    if (camera.current !== null) {
-      camera.current.aspect = width / height;
-      camera.current.updateProjectionMatrix();
+    // if (camera.current !== null) {
+    //   camera.current.aspect = width / height;
+    //   camera.current.updateProjectionMatrix();
+    // }
+    // renderer.current?.setSize(width, height);
+
+    if (camera.current instanceof THREE.PerspectiveCamera) {
+      camera.current.aspect = aspect;
+    } else if ( camera.current instanceof THREE.OrthographicCamera ){
+      camera.current.left = -1 * aspect;
+      camera.current.right = 1 * aspect;
     }
+
+    camera.current?.updateProjectionMatrix();
+
     renderer.current?.setSize(width, height);
+
   };
 
   /** 마우스 그래그로 회전시킴 */
@@ -140,6 +177,25 @@ const Camera = () => {
     const smallSpherePibot = scene.current?.getObjectByName("smallSpherePivot");
     if (smallSpherePibot) {
       smallSpherePibot.rotation.y = THREE.MathUtils.degToRad(time*50);
+      
+
+      // 카메라가 작은 구를 추적함 ( 작은 구가 바라보는 화면을 카메라로 전환하기 위해 )
+      const smallSphere = smallSpherePibot.children[0];
+      if (camera.current !== null ) {
+        smallSphere.getWorldPosition(camera.current.position);
+      }
+      // 작은 구보다 10도 더 회전시켜서 작은 구의 방향을 잡음
+      const targetPivot = scene.current?.getObjectByName("targetPivot");
+      if(targetPivot){
+        targetPivot.rotation.y = THREE.MathUtils.degToRad(time*50 + 10);
+
+        const target = targetPivot.children[0];
+        const pt = new THREE.Vector3();
+
+        // target을 카메라가 잡게 함
+        target.getWorldPosition(pt);
+        camera.current?.lookAt(pt);
+      }
     }
   };
 
