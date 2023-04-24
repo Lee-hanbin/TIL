58b02ecdb4311e618ae3306dc025b940a6450406
Lee-hanbin <hanbin0501@gmail.com>
2023-04-24 14:17:36 +0900

    :memo: 화면 렌더링 시, 객체 크기 조절
    - 객체의 크기에 따라 가득 차게 화면에 렌더링

--- 12. Threejs/threejs/src/components/09_Zoom.tsx
+++ 12. Threejs/threejs/src/components/09_Zoom.tsx
@@ -18,18 +18,63 @@ const Zoom = () => {
     const width = divContainer.current?.clientWidth || 0;
     const height = divContainer.current?.clientHeight || 0;
     const cam = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-    cam.position.z = 2;
+    cam.position.z = 6;
     camera.current = cam;
+
+    scene.current?.add(cam)
   };
 
   /** 조명 커스텀 함수 */
   const SetupLight = () => {
     const color = 0xffffff;
-    const intensity = 1;
+    const intensity = 10;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.current?.add(light);
+    light.position.set(-1, 3, 4);
+    // scene.current?.add(light);
+    // 카메라에 조명을 달았음
+    camera.current?.add(light)
   };
+
+  /** 카메라 적정 위치 구하는 함수 */
+  const ZoomFit = (object3D:any, camera:THREE.PerspectiveCamera) => {
+    // 모델 경계 박스
+    const box = new THREE.Box3().setFromObject(object3D);
+
+    // 모델 경계 박스 대각 길이
+    const sizeBox = box.getSize(new THREE.Vector3()).length();
+
+    // 모델의 경계 박스 중심 위치
+    const centerBox = box.getCenter(new THREE.Vector3());
+
+    // 모델 크기의 절반값
+    const halfSizeModel = sizeBox * 0.5;
+
+    // 카메라의 fov의 절반값
+    const halfFov = THREE.MathUtils.degToRad(camera.fov * 0.5);
+
+    // 모델을 화면에 꽉 채우기 위한 적당한 거리
+    const distance = halfSizeModel / Math.tan(halfFov);
+
+    // 모델 중심에서 카메라 위치로 향하는 방향 단위 벡터 계산
+    const direction = (new THREE.Vector3()).subVectors(
+      camera.position,
+      centerBox
+    ).normalize();
+
+    // "단위 방향 벡터" 방향으로 모델 중심 위치에서 distance 거리에 대한 위치
+    const position = direction.multiplyScalar(distance).add(centerBox);
+    camera.position.copy(position);
+
+    // 모델의 크기에 맞게 카메라의 near, far 값을 대략적으로 조정
+    camera.near = sizeBox / 100;
+    camera.far = sizeBox * 100;
+
+    // 카메라 기본 속성 변경에 따른 투영행렬 업데이트
+    camera.updateProjectionMatrix();
+
+    // 카메라가 모델의 중심을 바라 보도록 함
+    camera.lookAt(centerBox.x, centerBox.y, centerBox.z);
+  }
 
   /** 모델 커스텀 함수 */
   const SetupModel = () => {
@@ -39,6 +84,10 @@ const Zoom = () => {
       (glb) => {
         const root = glb.scene;
         scene.current?.add(root)
+
+        if (camera.current) {
+          ZoomFit(root, camera.current)
+        }
       }
     )
   };
--- 12. Threejs/threejs/src/routes/IntroPage.tsx
+++ 12. Threejs/threejs/src/routes/IntroPage.tsx
@@ -1,11 +1,11 @@
 import Basic from "../components/01_Basic";
+import Camera from "../components/07_Camera";
 import CustomGeometry from "../components/05_CustomGeometry";
 import Geometry from "../components/02_Geometry";
 import Light from "../components/06_Light";
 import Material from "../components/04_Material";
 import React from 'react';
 import SceneGraph from "../components/03_SceneGraph";
-import Camera from "../components/07_Camera";
 import Shadow from "../components/08_Shadow";
 import Zoom from "../components/09_Zoom";
 
