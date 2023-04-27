db922ee2dbc9cc17d87facaa70d25d1dc54d3f3b
Lee-hanbin <hanbin0501@gmail.com>
2023-04-27 18:00:53 +0900

    :memo: 줌인 줌아웃 기능 구현중

--- 12. Threejs/threejs/src/components/11_ZoomInOut.tsx
+++ 12. Threejs/threejs/src/components/11_ZoomInOut.tsx
@@ -2,14 +2,19 @@ import * as THREE from "three";
 
 import { useEffect, useRef } from "react";
 
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import grayCar from "../assets/lowpoly/carGray.glb"
+import greenCar from "../assets/lowpoly/carGoblin.glb"
+import redCar from "../assets/lowpoly/carRed.glb"
+import whiteCar from "../assets/lowpoly/carWhite.glb"
 
 const ZoomInOut = () => {
   const divContainer = useRef<HTMLDivElement>(null);
   const renderer = useRef<THREE.WebGLRenderer | null>(null);
   const scene = useRef<THREE.Scene | null>(null);
   const camera = useRef<THREE.PerspectiveCamera | null>(null);
-  const cube = useRef<THREE.Mesh | null>(null);
+
   const controls = useRef<OrbitControls |null>(null);
 
   /** 카메라 커스텀 함수 */
@@ -23,22 +28,57 @@ const ZoomInOut = () => {
 
   /** 조명 커스텀 함수 */
   const SetupLight = () => {
+    const ambienttLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.4);
+    scene.current?.add(ambienttLight);
+
     const color = 0xffffff;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.current?.add(light);
+    const intensity = 1.5;
+
+    const light1 = new THREE.DirectionalLight(color, intensity);
+    light1.position.set(-1, 2, 0);
+    scene.current?.add(light1);
+
+    const light2 = new THREE.DirectionalLight(color, intensity);
+    light2.position.set(1, 4, 0);
+    scene.current?.add(light2);
   };
 
   /** 모델 커스텀 함수 */
   const SetupModel = () => {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshPhongMaterial({ color: 0x44a88 });
+    const gltfLoader = new GLTFLoader();
+    const items = [
+      {url: whiteCar},
+      {url: grayCar}
+    ]
 
-    const mesh = new THREE.Mesh(geometry, material);
+    items.forEach((item, index) => {
+      gltfLoader.load(item.url, (glb) => {
+        const obj3d = glb.scene;
 
-    scene.current?.add(mesh);
-    cube.current = mesh;
+        const box = new THREE.Box3().setFromObject(obj3d);
+        const sizeBox = box.max.z - box.min.z;
+        const scale = 1 / sizeBox;
+        const tx = (index / (items.length-1)) - 0.5;
+        obj3d.scale.set(scale, scale, scale);
+        obj3d.position.set(tx, -box.min.y*scale, 0);
+
+        scene.current?.add(obj3d);
+        obj3d.name = "car"
+
+        scene.current?.add(new THREE.BoxHelper(obj3d))
+      })
+    })
+
+    const boxGeometry = new THREE.CylinderGeometry(1.2, 1.2, 0.1, 64);
+    const boxMaterial = new THREE.MeshStandardMaterial({
+      color: 0x454545,
+      metalness: 0.5,
+      roughness: 0.5
+    });
+    const box = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    box.position.set(0, -0.05, -0.3)
+    scene.current?.add(box)
   };
 
   /** 렌더링 될 때마다 사이즈 초기화 */
@@ -69,8 +109,7 @@ const ZoomInOut = () => {
 
   const update = (time: number) => {
     time *= 0.01;
-    cube.current!.rotation.x = time;
-    cube.current!.rotation.y = time;
+
   };
 
   useEffect(() => {
@@ -78,6 +117,8 @@ const ZoomInOut = () => {
       const ren = new THREE.WebGLRenderer({ antialias: true });
       ren.setPixelRatio(window.devicePixelRatio);
       divContainer.current.appendChild(ren.domElement);
+      ren.shadowMap.enabled = true;
+      ren.shadowMap.type = THREE.PCFSoftShadowMap;
       renderer.current = ren;
 
       const scn = new THREE.Scene();
